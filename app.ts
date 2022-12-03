d27222a177b5da4e408df3ad5a7e9e5b0ec813ea
samyar <75088294+samyarkd@users.noreply.github.com>
2022-12-03 07:58:37 +0000

    add scripts to app

---
+++ app.ts
@@ -0,0 +1,6 @@
+import { names } from "./test";
+
+const numbers: number[] = [1, 2];
+
+console.log(numbers);
+console.log(names);
---
+++ test.js
@@ -0,0 +1,4 @@
+"use strict";
+exports.__esModule = true;
+exports.names = void 0;
+exports.names = ['samyar', 'omid'];
